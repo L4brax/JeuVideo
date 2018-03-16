2dca7896dd873dfb2bf8a8ba99bf3f3ced8c4251
AlexLP <alexlepoupon@gmail.com>
2018-03-16 17:56:07 +0100

    add forget file

---
+++ Assets/AlexandreLePoupon/Script/TouchByMob.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchByMob : MonoBehaviour {
+
+	private void OnTriggerEnter2D (Collider2D other) {
+		if(other.gameObject.name == "Playermob1") {
+			Debug.Log("Touché");
+		}
+		if(other.gameObject.name == "Playermob2") {
+			Debug.Log("Touché");
+		}
+		if(other.gameObject.name == "Playermob3") {
+			Debug.Log("Touché");
+		}
+	}
+}
